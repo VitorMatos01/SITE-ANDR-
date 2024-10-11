01522661c476cd5d8dff02e342083bf035f2b98c
VitorMatos01 <vitorandre735@gmail.com>
2024-10-11 12:20:38 -0300

    Add files via upload

---
+++ André Vitor Tavares/anotações_dev_web.py
@@ -0,0 +1,9 @@
+'''<a> ancora, indica a mudança de seção. Já o href especifica isso
+ou seja, direciona pra algum lugar especifico que foi
+escrito no código
+um (hyperlink)'''
+
+'''ficou azul por add um link'''
+
+
+'''div deixar a estrutura organizada só'''
